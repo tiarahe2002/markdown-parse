923bdb28e691bb1c0900f7231f4ca2853d5e954b
tiarahe2002 <97692867+tiarahe2002@users.noreply.github.com>
2022-03-09 19:07:32 -0800

    update

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -1,8 +1,58 @@
 import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
+import java.util.ArrayList;
 
-Parser parser = Parser.builder().build();
-Node document = parser.parse("This is *Sparta*");
-HtmlRenderer renderer = HtmlRenderer.builder().build();
-renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+class TryCommonMark {
+    public static void main(String[] args) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse("This is *Sparta*");
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        //System.out.print(renderer.render(document));
+        // "<p>This is <em>Sparta</em></p>\n"
+        
+        //this part actually does the computation
+        Node node = parser.parse("[link](abc.com)[link](abc.com)");
+        LinkCountVisitor visitor = new LinkCountVisitor();
+        node.accept(visitor);
+        System.out.println(visitor.links);  // 4
+    }
+}
+
+
+ 
+//this class can be defined anywhere in the file
+class WordCountVisitor extends AbstractVisitor {
+    int wordCount = 0;
+ 
+    @Override
+    public void visit(Text text) {
+        // This is called for all Text nodes. Override other visit methods for other node types.
+ 
+        // Count words (this is just an example, don't actually do it this way for various reasons).
+        wordCount += text.getLiteral().split("\\W+").length;
+ 
+        // Descend into children (could be omitted in this case because Text nodes don't have children).
+        visitChildren(text);
+    }
+}
+
+//this class can be defined anywhere in the file
+class LinkCountVisitor extends AbstractVisitor {
+    int linkCount = 0;
+    ArrayList<String> links = new ArrayList<>();
+ 
+    @Override
+    public void visit(Link link) {
+        // This is called for all Link nodes.
+
+        // Count links
+        linkCount += 1;
+
+        //add link to list
+        links.add(link.getDestination());
+ 
+        // Descend into children
+        visitChildren(link);
+    }
+}
--- makefile
+++ makefile
@@ -1,13 +1,13 @@
 CLASSPATH = lib/*:.
 
-MarkdownParseTest.class: MarkdownParseTest.java MarkdownParse.class
-	javac -g -cp $(CLASSPATH) MarkdownParseTest.java
-
 MarkdownParse.class: MarkdownParse.java
-	javac -g -cp $(CLASSPATH) MarkdownParse.java
+	javac -cp $(CLASSPATH) MarkdownParse.java
 
-test: MarkdownParseTest.class
-	java -cp $(CLASSPATH) org.junit.runner.JUnitCore MarkdownParseTest 
+MarkdownParseTest.class: MarkdownParseTest.java
+	javac -cp $(CLASSPATH) MarkdownParseTest.java
+
+test: MarkdownParse.class MarkdownParseTest.class
+	java -cp $(CLASSPATH) org.junit.runner.JUnitCore MarkdownParseTest
 
 TryCommonMark.class: TryCommonMark.java
 	javac -g -cp $(CLASSPATH) TryCommonMark.java
